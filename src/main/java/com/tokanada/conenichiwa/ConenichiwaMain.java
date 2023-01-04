fbf000fdb5355110d152f0b258b8e030c1c690b2
tokanada <davidt.3200@gmail.com>
2023-01-04 01:19:55 -0800

    Refactor Warnings

--- src/main/java/com/tokanada/conenichiwa/ConenichiwaMain.java
+++ src/main/java/com/tokanada/conenichiwa/ConenichiwaMain.java
@@ -4,15 +4,11 @@ import net.fabricmc.api.ModInitializer;
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class ConenichiwaMain implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
-
 	public static final Identifier CONE1 = new Identifier("conenichiwa:cone1");
 	public static SoundEvent CONE1_EVENT = new SoundEvent(CONE1);
 
